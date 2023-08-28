87f947bea77255a6e8c738627d59e7ffb60b6fcc
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-08-28 21:08:17 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -113,7 +113,7 @@ async def gen_thumb(videoid, user_id):
         width = int((1280 - 1150) / 2)
         background = Image.open(f"cache/temp{videoid}.png")
         background.paste(logo, (width + 2, 70), mask=logo)
-        background.paste(x, (840, 360), mask=x)
+        background.paste(x, (840, 20), mask=x)
         background.paste(image3, (0, 0), mask=image3)
 
         draw = ImageDraw.Draw(background)
